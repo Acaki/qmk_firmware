3bbad27a5e1fef7ce1960c7f7e434b789f4baf4f
Acaki <axsz4298@gmail.com>
2019-08-07 12:14:05 +0800

    Make left gui one shot

--- keyboards/dz60/keymaps/mine/keymap.c
+++ keyboards/dz60/keymaps/mine/keymap.c
@@ -11,7 +11,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPC,
 	        KC_ESC, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
 	        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, RSFT_T(KC_SLSH), KC_UP, KC_MPLY,
-	        KC_LCTL, KC_LGUI, OSM(MOD_LALT), LT(2,KC_SPC), OSM(MOD_RCTL), MO(2), KC_RALT, MO(2), KC_LEFT, KC_DOWN, KC_RGHT
+	        KC_LCTL, OSM(MOD_LGUI), OSM(MOD_LALT), LT(2,KC_SPC), OSM(MOD_RCTL), MO(2), KC_RALT, MO(2), KC_LEFT, KC_DOWN, KC_RGHT
 	        ),
     [1] = LAYOUT_60_b_ansi(
             _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
