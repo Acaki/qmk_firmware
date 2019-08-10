49b999d969e67d0930794d0f05f22cda5086b61f
Acaki <axsz4298@gmail.com>
2019-08-10 16:24:27 +0800

    update

--- keyboards/dz60/keymaps/mine/keymap.c
+++ keyboards/dz60/keymaps/mine/keymap.c
@@ -11,14 +11,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPC,
 	        KC_ESC, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
 	        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, RSFT_T(KC_SLSH), KC_UP, KC_MPLY,
-	        KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_RCTL, MO(1), KC_RALT, MO(1), KC_LEFT, KC_DOWN, KC_RGHT
+	        KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, LT(1, KC_ENT), KC_BSPC, KC_RALT, MO(1), KC_LEFT, KC_DOWN, KC_RGHT
 	        ),
     [1] = LAYOUT_60_b_ansi(
 	        _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, _______, RESET,
-	        KC_HOME, KC_PGUP, KC_UP, KC_PGDN, _______, DBLARROW, _______, _______, _______, _______, _______, _______, _______, _______,
-	        KC_END, KC_LEFT, KC_DOWN, KC_RIGHT, KC_BSPC, ARROW, _______, KC_LBRC, KC_RBRC, KC_MINS, KC_EQL, _______, _______,
+	        _______, _______, _______, _______, _______, _______, DBLARROW, KC_HOME, KC_UP, KC_END, KC_PGUP, _______, _______, _______,
+	        _______, _______, _______, _______, _______, _______, ARROW, KC_LEFT, KC_DOWN, KC_RIGHT, KC_PGDN, _______, _______,
 	        _______, _______, KC_CALC, _______, _______, _______, _______, _______, _______, _______, _______, KC_VOLU, _______,
-	        _______, _______, _______, KC_ENT, TG(2), _______, MO(3), _______, KC_MPRV, KC_VOLD, KC_MNXT
+	        _______, _______, _______, _______, _______, TG(2), MO(3), _______, KC_MPRV, KC_VOLD, KC_MNXT
 	        ),
     [2] = LAYOUT_60_b_ansi(
             _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
@@ -33,11 +33,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	        _______, _______, RGB_HUI, RGB_HUD, RGB_SPI, RGB_SPD, _______, _______, _______, _______, _______, _______, _______,
 	        _______, _______, RGB_SAI, RGB_SAD, _______, _______, _______, _______, _______, _______, _______, _______, _______,
 	        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
-	        )
-};
+                )
+        };
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    switch (keycode) {
+        bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+        switch (keycode) {
         case ARROW:
             if (record->event.pressed) {
                 SEND_STRING("->");
