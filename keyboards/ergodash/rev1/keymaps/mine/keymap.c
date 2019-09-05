74f8af50ad27ff5931563f7221ba47199af3af57
Acaki <axsz4298@gmail.com>
2019-09-05 18:26:19 +0800

    update

--- keyboards/ergodash/rev1/keymaps/mine/keymap.c
+++ keyboards/ergodash/rev1/keymaps/mine/keymap.c
@@ -3,8 +3,8 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT_4key(
             KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_MINS, KC_EQL, KC_6, KC_7, KC_8, KC_9, KC_0, KC_DEL,
-            KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_LBRC, KC_RBRC, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
-            KC_ESC, KC_A, KC_S, KC_D, KC_F, KC_G, KC_QUOT, KC_BSLS, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_ENT,
+            KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_LBRC, KC_RBRC, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS,
+            KC_ESC, KC_A, KC_S, KC_D, KC_F, KC_G, KC_ENT, KC_QUOT, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_ENT,
             KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_LCTL, KC_RCTL, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, TG(2),
             KC_LCTL, KC_APP, KC_LGUI, KC_LALT, KC_SPC, KC_LSFT, MO(1), MO(1), KC_RSFT, KC_BSPC, KC_RALT, KC_RGUI, KC_APP, KC_MPLY
 	        ),
