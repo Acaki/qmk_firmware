a51bd2af9d2c974694bf03424248a7e02e6d5bc0
Acaki <axsz4298@gmail.com>
2019-09-28 18:55:33 +0800

    update

--- keyboards/keebio/iris/keymaps/mine/config.h
+++ keyboards/keebio/iris/keymaps/mine/config.h
@@ -26,3 +26,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_HUE_STEP 8
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
+
+#define MOUSEKEY_DELAY 150
+#define MOUSEKEY_INTERVAL 6
+#define MOUSEKEY_TIME_TO_MAX 250
+#define MOUSEKEY_MAX_SPEED 2
+#define MOUSEKEY_WHEEL_MAX_SPEED 1
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 0
+#define ONESHOT_TAP_TOGGLE 2
+#define ONESHOT_TIMEOUT 3000
