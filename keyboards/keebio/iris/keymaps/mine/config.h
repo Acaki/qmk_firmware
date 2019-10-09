3bc9038a61631260c03f2a8503d37e0ea8d83743
Acaki <axsz4298@gmail.com>
2019-10-09 23:56:21 +0800

    Revert "update"
    This reverts commit 58a77724

--- keyboards/keebio/iris/keymaps/mine/config.h
+++ keyboards/keebio/iris/keymaps/mine/config.h
@@ -33,4 +33,3 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MOUSEKEY_MAX_SPEED 2
 #define MOUSEKEY_WHEEL_MAX_SPEED 1
 #define MOUSEKEY_WHEEL_TIME_TO_MAX 0
-#define TAPPING_TERM 125
