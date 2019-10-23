1af11db6c49935e22acb5207f2c50f6910c70c9d
Acaki <axsz4298@gmail.com>
2019-10-23 17:55:15 +0800

    update

--- keyboards/keebio/iris/keymaps/mine/config.h
+++ keyboards/keebio/iris/keymaps/mine/config.h
@@ -34,3 +34,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MOUSEKEY_WHEEL_MAX_SPEED 1
 #define MOUSEKEY_WHEEL_TIME_TO_MAX 0
 #define FORCE_NKRO
+#define PERMISSIVE_HOLD
+#define TAPPING_TERM 125
