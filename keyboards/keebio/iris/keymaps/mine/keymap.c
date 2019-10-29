69b86a3db0e3743d9f8b286b543d55f6191412db
Acaki <axsz4298@gmail.com>
2019-10-30 00:24:49 +0800

    update

--- keyboards/keebio/iris/keymaps/mine/keymap.c
+++ keyboards/keebio/iris/keymaps/mine/keymap.c
@@ -8,7 +8,8 @@ extern keymap_config_t keymap_config;
 #define ARROWS TG(_ARROWS)
 #define RETURN TO(_QWERTY)
 #define ALTESC LALT_T(KC_ESC)
-#define SFTENT RSFT_T(KC_ENT)
+#define SFTBSPC RSFT_T(KC_BSPC)
+#define ALTENT RALT_T(KC_ENT)
 enum layers {
     _QWERTY,
     _LOWER,
@@ -32,11 +33,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_RCTL,
+     ALTESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, ALTENT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    LOWER,            RAISE,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RGUI,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    ALTESC,  KC_SPC,  LOWER,                     RAISE,   SFTENT,  KC_BSPC
+                                    KC_LCTL,  KC_SPC,  LOWER,                    RAISE,   SFTBSPC, KC_RCTL
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
