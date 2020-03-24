01cea3cbac99d26a6293d318331ab7ece6262bba
Acaki <axsz4298@gmail.com>
2020-03-25 00:51:24 +0800

    update

--- keyboards/kyria/keymaps/mine/keymap.c
+++ keyboards/kyria/keymaps/mine/keymap.c
@@ -13,6 +13,7 @@ enum layers {
     _QWERTY,
     _GAMING,
     _MOUSE,
+    _NUMBER,
     _LOWER,
     _RAISE,
     _ADJUST
@@ -29,8 +30,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      [_QWERTY] = LAYOUT(
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,                                            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
         KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,     KC_G,                                            KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_RSFT,
-        KC_GRV,  KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,    KC_ESC,  KC_LGUI,     KC_RGUI, KC_BSPC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
-                                   KC_LALT, KC_LCTL,  KC_SPC,  LOWER,   MOUSE,       KC_CAPS, RAISE,   KC_ENT,  KC_RCTL, KC_RALT
+        KC_LGUI, KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,    NUMBER, MOUSE,        KC_APP,  NUMBER,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RGUI,
+                                   KC_LALT, KC_LCTL,  KC_SPC,  LOWER,  KC_ESC,       KC_BSPC, RAISE,   KC_ENT,  KC_RCTL, KC_RALT
      ),
 
     [_GAMING] = LAYOUT(
@@ -47,24 +48,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                    _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______
     ),
 
-    [_LOWER] = LAYOUT(
+
+    [_NUMBER] = LAYOUT(
         _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-        _______, _______, ASFT,    ATAB,    KC_APP,  GAMING,                                          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
-        KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, _______,     _______, _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
+        KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                                           KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+        _______, _______, _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______, _______, _______,
+                                   _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______
+    ),
+
+
+    [_LOWER] = LAYOUT(
+        _______, _______, _______, _______, _______, _______,                                         KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSLS,
+        _______, _______, ASFT,    ATAB,    KC_CAPS, GAMING,                                          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, KC_PIPE,
+        _______, _______, _______, _______, _______, _______, _______, _______,     _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______,
                                    _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______
     ),
 
     [_RAISE] = LAYOUT(
-        _______, KC_LBRC, KC_PLUS, KC_DQT,  KC_MINS, KC_RBRC,                                          _______, _______, _______, _______, _______, _______,
-        _______, KC_LCBR ,KC_EQL,  KC_QUOT, KC_UNDS, KC_RCBR,                                          KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_MPLY, _______,
-        _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______, _______,      _______, _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+        KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                                          _______, _______, _______, _______, _______, _______,
+        KC_TILD, KC_LCBR ,KC_EQL,  KC_QUOT, KC_UNDS, KC_RCBR,                                          KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
+        _______, KC_LBRC, KC_PLUS, KC_DQT,  KC_MINS, KC_RBRC, _______, _______,      _______, _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, KC_MPLY, _______,
                                    _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______
     ),
 
     [_ADJUST] = LAYOUT(
         RSTROM,  _______, _______, _______, _______, _______,                                          _______, _______, _______, _______, _______, _______,
-        _______, _______, DBLARR,  _______, ARROW,   _______,                                          KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______,
-        _______, _______, _______, _______, _______, RESET,   _______, _______,      _______, _______, _______, KC_BRID, KC_BRIU, _______, _______, _______,
+        _______, _______, DBLARR,  _______, ARROW,   _______,                                          _______, KC_BRID, KC_BRIU, _______, _______, _______,
+        _______, _______, _______, _______, _______, RESET,   _______, _______,      _______, _______, _______, _______, _______, _______, _______, _______,
                                    _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______
     ),
 };
@@ -113,6 +123,9 @@ static void render_status(void) {
             break;
         case _MOUSE:
             oled_write_P(PSTR("Mouse\n"), false);
+            break;
+        case _NUMBER:
+            oled_write_P(PSTR("Number\n"), false);
             break;
         case _LOWER:
             oled_write_P(PSTR("Lower\n"), false);
