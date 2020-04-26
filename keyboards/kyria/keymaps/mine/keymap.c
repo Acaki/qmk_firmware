7673b58398d3a6b11a4c89874d1e8589803f65db
Acaki <axsz4298@gmail.com>
2020-04-26 18:50:03 +0800

    update

--- keyboards/kyria/keymaps/mine/keymap.c
+++ keyboards/kyria/keymaps/mine/keymap.c
@@ -4,7 +4,7 @@ extern keymap_config_t keymap_config;
 
 extern uint8_t is_master;
 
-#define MOUSE TG(_MOUSE)
+#define MOUSE MO(_MOUSE)
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
 #define GAMING TG(_GAMING)
@@ -26,8 +26,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      [_QWERTY] = LAYOUT(
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,                                            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
         KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,     KC_G,                                            KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,    _______, _______,     _______, _______, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                                   KC_LGUI, KC_LALT,  KC_LCTL, KC_SPC,  LOWER,       RAISE,   KC_BSPC, KC_RCTL, KC_RALT, KC_RGUI
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,    KC_LALT, GAMING,      KC_APP,  KC_RALT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                                   KC_LGUI, KC_LCTL,  KC_SPC,  LOWER,   MOUSE,       KC_CAPS, RAISE,   KC_BSPC, KC_RCTL, KC_RGUI
      ),
 
     [_GAMING] = LAYOUT(
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_LOWER] = LAYOUT(
-        _______, _______, _______, KC_APP,  MOUSE,   GAMING,                                          KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSLS,
+        _______, _______, _______, _______, _______, _______,                                         KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_BSLS,
         KC_F12,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_PIPE,
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, _______,     _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,
                                    _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______
