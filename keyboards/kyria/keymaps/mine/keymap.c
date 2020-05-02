713492a3f5316318239351e70684744dd8e5f2fd
Acaki <axsz4298@gmail.com>
2020-05-02 17:05:38 +0800

    update

--- keyboards/kyria/keymaps/mine/keymap.c
+++ keyboards/kyria/keymaps/mine/keymap.c
@@ -31,8 +31,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      [_QWERTY] = LAYOUT(
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,                                            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
         KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,     KC_G,                                            KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_RSFT,
-        KC_APP,  KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,    KC_LALT, MOUSE,       KC_CAPS, KC_RALT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_MPLY,
-                                   KC_LGUI, KC_LCTL,  KC_SPC,  LOWER,   KC_ESC,      KC_BSPC, RAISE,   KC_ENT,  KC_RCTL, KC_RGUI
+        KC_APP,  KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,    KC_ESC, MOUSE,        KC_CAPS, KC_BSPC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_MPLY,
+                                   KC_LGUI, KC_LCTL,  KC_SPC,  LOWER,  KC_LALT,      KC_RALT, RAISE,   KC_ENT,  KC_RCTL, KC_RGUI
      ),
 
     [_QWERTY_MAC] = LAYOUT(
