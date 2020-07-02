ec3c3f6d46ac6e1e6c23d7e079f6f77af6a4a773
Mason <mason.chen@cinnamon.is>
2020-07-03 00:00:12 +0800

    Update for macos

--- keyboards/kyria/keymaps/mine/keymap.c
+++ keyboards/kyria/keymaps/mine/keymap.c
@@ -28,9 +28,9 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      [_QWERTY] = LAYOUT(
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,                                            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
-        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,     KC_G,                                            KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_RCTL,
-        KC_APP,  KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,    KC_LALT, MOUSE,       KC_CAPS, KC_RALT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_MPLY,
-                                   KC_LGUI, KC_LSFT,  KC_SPC,  LOWER,   KC_ESC,      KC_BSPC, RAISE,   KC_ENT,  KC_RSFT, KC_RGUI
+        KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,     KC_G,                                            KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_RSFT,
+        KC_GRV,  KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,    KC_LALT, MOUSE,       KC_CAPS, KC_RALT, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
+                                   KC_LGUI, KC_LCTL,  KC_SPC,  LOWER,   KC_ESC,      KC_BSPC, RAISE,   KC_ENT,  KC_RCTL, KC_RGUI
      ),
 
     [_QWERTY_MAC] = LAYOUT(
@@ -56,20 +56,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_LOWER] = LAYOUT(
         _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-        _______, _______, ATAB,    ASFT,    _______, GAMING,                                          KC_HOME, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_END,
-        KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, _______,     KC_PGUP, _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-                                   _______, _______, _______, _______, _______,     KC_PGDN, _______, _______, _______, _______
+        _______, KC_6,    KC_7,    KC_8,    ATAB,    GAMING,                                          KC_PGDN, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_UP,
+        KC_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, _______,     KC_HOME, _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,
+                                   _______, _______, _______, _______, _______,     KC_END,  _______, _______, _______, _______
     ),
 
     [_RAISE] = LAYOUT(
         KC_AMPR, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, _______,                                         _______, _______, KC_DQT,  KC_PLUS, _______, _______,
-        KC_ASTR, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_TILD,                                         KC_PIPE, KC_UNDS, KC_QUOT, KC_EQL,  KC_COLN, _______,
-        _______, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_GRV,  _______, _______,     _______, _______, KC_BSLS, KC_MINS, KC_LABK, KC_RABK, KC_QUES, _______,
+        KC_ASTR, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, _______,                                         _______, KC_UNDS, KC_QUOT, KC_EQL,  KC_COLN, _______,
+        KC_TILD, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, _______, _______, _______,     _______, _______, _______, KC_MINS, KC_LABK, KC_RABK, KC_QUES, KC_PIPE,
                                    _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______
     ),
 
     [_ADJUST] = LAYOUT(
-        RSTROM,  _______, WINDOWS, _______, RESET,   _______,                                          _______, _______, _______, _______, _______,  _______,
+        RSTROM,  _______, WINDOWS, _______, RESET,   _______,                                          _______, _______, _______, _______, KC_MPLY, _______,
         _______, _______, _______, _______, _______, _______,                                          _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, _______,
         _______, _______, _______, _______, _______, _______, _______, _______,      _______, _______, _______, MACOS,   _______, _______, _______, _______,
                                    _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______
