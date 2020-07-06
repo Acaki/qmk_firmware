8bcd657f68e8c89dd34f95ba62c71ee08f762b0d
Mason <mason.chen@cinnamon.is>
2020-07-06 16:17:33 +0800

    update

--- keyboards/kyria/keymaps/mine/keymap.c
+++ keyboards/kyria/keymaps/mine/keymap.c
@@ -47,9 +47,9 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      [_QWERTY] = LAYOUT(
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,     KC_T,                                            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
-        KC_ESC,  A_GUI,   S_ALT,   D_SFT,   F_CTL,    KC_G,                                            KC_H,    J_CTL,   K_SFT,   L_ALT,   SC_GUI,  KC_QUOT,
-        KC_GRV,  KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,    MOUSE, KC_VOLU,       KC_MPRV, KC_CAPS, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
-                                   GAMING,  NAV,      KC_SPC,  LOWER, KC_VOLD,       KC_MNXT, RAISE,   KC_ENT,  KC_BSPC, KC_MPLY
+        KC_LSFT, A_GUI,   S_ALT,   D_SFT,   F_CTL,    KC_G,                                            KC_H,    J_CTL,   K_SFT,   L_ALT,   SC_GUI,  KC_QUOT,
+        KC_GRV,  KC_Z,    KC_X,    KC_C,    KC_V,     KC_B,    MOUSE, KC_PGUP,       KC_HOME, KC_CAPS, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
+                                   GAMING,  KC_ESC,   KC_SPC,  LOWER, KC_PGDN,       KC_END,  RAISE,   KC_ENT,  KC_BSPC, KC_MPLY
      ),
 
     [_QWERTY_MAC] = LAYOUT(
@@ -74,29 +74,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_LOWER] = LAYOUT(
-        _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,                                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
         _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                                            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-        KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, _______,     _______, _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+        KC_F11,  KC_6,    KC_7,    KC_8,    ATAB,    _______,                                         _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_F12,
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, _______,     _______, _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
                                    _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______
     ),
 
     [_RAISE] = LAYOUT(
-        _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                                         _______, _______, _______, _______, _______, _______,
-        _______, KC_LCBR, KC_LBRC, KC_EQL,  KC_UNDS, _______,                                         _______, _______, _______, _______, _______, _______,
-        _______, KC_RCBR, KC_RBRC, KC_PLUS, KC_MINS, _______, _______, _______,     _______, _______, _______, _______, _______, _______, _______, _______,
-                                   _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______
-    ),
-
-    [_NAV] = LAYOUT(
-        ATAB,    _______, _______, _______, _______, _______,                                         KC_INS,  KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______,
-        _______, _______, _______, KC_LSFT, _______, _______,                                         KC_PSCR, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                                         KC_CIRC, KC_AMPR, KC_ASTR, _______, _______, _______,
+        _______, KC_LCBR, KC_LBRC, KC_LPRN, KC_UNDS, KC_EQL,                                          _______, _______, _______, _______, _______, _______,
+        _______, KC_RCBR, KC_RBRC, KC_RPRN, KC_MINS, KC_PLUS, _______, _______,     _______, _______, _______, _______, _______, _______, _______, _______,
                                    _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______
     ),
 
     [_ADJUST] = LAYOUT(
         RSTROM,  _______, WINDOWS, _______, RESET,   _______,                                          _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______,                                          _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______,                                          _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, _______,
         _______, _______, _______, _______, _______, _______, _______, _______,      _______, _______, _______, MACOS,   _______, _______, _______, _______,
                                    _______, _______, _______, _______, _______,      _______, _______, _______, _______, _______
     ),
