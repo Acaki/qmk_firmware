48d96b92cd5c4edafd0174987cdcf50404c3edde
Mason <mason.chen@cinnamon.is>
2020-08-24 16:20:56 +0800

    Disable oled

--- keyboards/kyria/keymaps/mine/rules.mk
+++ keyboards/kyria/keymaps/mine/rules.mk
@@ -1,4 +1,4 @@
-OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
+OLED_DRIVER_ENABLE = no   # Enables the use of OLED displays
 RGBLIGHT_ENABLE = no
 BOOTLOADER = atmel-dfu
 NKRO_ENABLE = yes
