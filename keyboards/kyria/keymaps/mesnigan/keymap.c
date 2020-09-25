58957275a8a3415db99a753970ff4ac4da66a3c8
Mason <mason.chen@cinnamon.is>
2020-09-25 11:58:35 +0800

    update

--- keyboards/kyria/keymaps/mesnigan/keymap.c
+++ keyboards/kyria/keymaps/mesnigan/keymap.c
@@ -8,9 +8,9 @@
               K31, K32, K33, K34, K35, K36 \
   ) \
   LAYOUT_wrapper( \
-      KC_TAB,  K01,     K02,     K03,     K04,     K05,                                             K06,     K07,     K08,     K09,     K0A,     KC_DEL, \
-      KC_LSFT, K11,     K12,     K13,     K14,     K15,                                             K16,     K17,     K18,     K19,     K1A,     KC_RSFT, \
-      KC_GRV,  K21,     K22,     K23,     K24,     K25,    KC_LALT, _______,    _______,  KC_RALT,  K26,     K27,     K28,     K29,     K2A,     KC_BSLS, \
+      KC_TAB,  K01,     K02,     K03,     K04,     K05,                                             K06,     K07,     K08,     K09,     K0A,     KC_DEL,  \
+      KC_ESC,  K11,     K12,     K13,     K14,     K15,                                             K16,     K17,     K18,     K19,     K1A,     KC_BSPC, \
+      KC_LSFT, K21,     K22,     K23,     K24,     K25,    KC_LALT, _______,    _______,  KC_RALT,  K26,     K27,     K28,     K29,     K2A,     KC_RSFT, \
                                  KC_LGUI, K31,     K32,    K33,     MOUSE,      KC_APP,   K34,      K35,     K36,     KC_RGUI \
     )
 /* Re-pass though to allow templates to be used */
