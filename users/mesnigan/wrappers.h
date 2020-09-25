6cd3d7da2da9c7ce78e44328a06dba9289d0fb57
Mason <mason.chen@cinnamon.is>
2020-09-25 11:19:13 +0800

    update

--- users/mesnigan/wrappers.h
+++ users/mesnigan/wrappers.h
@@ -27,12 +27,12 @@ NOTE: These are all the same length.  If you do a search/replace
 #define _________________QWERTY_L1_________________        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T
 #define _________________QWERTY_L2_________________        KC_A,    KC_S,    KC_D,    KC_F,    KC_G
 #define _________________QWERTY_L3_________________        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B
-#define _________________QWERTY_LT_________________                                   KC_LCTL, KC_SPC,  LOWER 
+#define _________________QWERTY_LT_________________                                   KC_LCTL, KC_SPC,  LT(_LOWER, KC_ESC)
 
 #define _________________QWERTY_R1_________________                 KC_Y,    KC_U,    KC_I,    KC_O,    KC_P
 #define _________________QWERTY_R2_________________                 KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN
 #define _________________QWERTY_R3_________________                 KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH
-#define _________________QWERTY_RT_________________        RAISE,   KC_ENT,  KC_RCTL
+#define _________________QWERTY_RT_________________   LT(_RAISE, KC_BSPC),   KC_ENT,  KC_RCTL
 
 #define _________________GAMING_L1_________________        KC_T,    KC_Q,    KC_X,    KC_E,    KC_R
 #define _________________GAMING_L2_________________        KC_G,    KC_A,    KC_W,    KC_D,    KC_F
@@ -51,21 +51,21 @@ NOTE: These are all the same length.  If you do a search/replace
 #define ___________________BLANK___________________        _______, _______, _______, _______, _______
 
 
-#define _________________LOWER_L1__________________        _______, ATAB,    ASFT,    _______, _______
-#define _________________LOWER_L2__________________        ________________NUMBER_LEFT________________
+#define _________________LOWER_L1__________________        ________________NUMBER_LEFT________________
+#define _________________LOWER_L2__________________        _______, KC_CAPS, ASFT,    ATAB,    _______
 #define _________________LOWER_L3__________________        _________________FUNC_LEFT_________________
 
-#define _________________LOWER_R1__________________        _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
-#define _________________LOWER_R2__________________        ________________NUMBER_RIGHT_______________
+#define _________________LOWER_R1__________________        ________________NUMBER_RIGHT_______________
+#define _________________LOWER_R2__________________        KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______
 #define _________________LOWER_R3__________________        _________________FUNC_RIGHT________________
 
 #define _________________RAISE_L1__________________        KC_PERC, KC_AT,   KC_EXLM, KC_HASH, KC_DLR
-#define _________________RAISE_L2__________________        KC_LCBR, KC_LBRC, KC_LPRN, KC_UNDS, KC_GRV
-#define _________________RAISE_L3__________________        KC_RCBR, KC_RBRC, KC_RPRN, KC_MINS, KC_TILD
+#define _________________RAISE_L2__________________        KC_LCBR, KC_LBRC, KC_LPRN, KC_UNDS, _______
+#define _________________RAISE_L3__________________        KC_RCBR, KC_RBRC, KC_RPRN, KC_MINS, _______
 
 #define _________________RAISE_R1__________________        KC_ASTR, KC_AMPR, _______, KC_CIRC, _______
-#define _________________RAISE_R2__________________        KC_BSLS, KC_QUOT, KC_DQT,  KC_EQL,  KC_PLUS
-#define _________________RAISE_R3__________________        KC_PIPE, _______, _______, _______, _______
+#define _________________RAISE_R2__________________        _______, KC_QUOT, KC_DQT,  KC_EQL,  KC_PLUS
+#define _________________RAISE_R3__________________        _______, _______, _______, _______, _______
 
 #define _________________ADJUST_L1_________________        RSTROM,  _______, _______, RESET,   _______
 #define _________________ADJUST_L2_________________        _______, KC_PGUP, KC_HOME, KC_PGDN, GAMING
