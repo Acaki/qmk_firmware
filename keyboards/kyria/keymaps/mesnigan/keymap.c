b3c54f3915621085f8a5cec87e8848b93216700c
Acaki <github-20201224@wdy.one>
2021-02-19 15:08:31 +0800

    Add encoder for kyria

--- keyboards/kyria/keymaps/mesnigan/keymap.c
+++ keyboards/kyria/keymaps/mesnigan/keymap.c
@@ -67,3 +67,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _________________QWERTY_LT_________________, _________________QWERTY_RT_________________
     ),
 };
+
+void encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) { /* First encoder */
+        if (clockwise) {
+            tap_code(KC_WH_U);
+        } else {
+            tap_code(KC_WH_D);
+        }
+    } else if (index == 1) { /* Second encoder */
+        if (clockwise) {
+            tap_code(KC_VOLD);
+        } else {
+            tap_code(KC_VOLU);
+        }
+    }
+}
