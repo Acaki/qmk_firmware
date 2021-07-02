93cc3adf91d856ecd1e364c4a713d00787add68d
Mason <mason.chen@cinnamon.is>
2021-07-02 18:08:45 +0800

    update

--- keyboards/kyria/keymaps/mesnigan/keymap.c
+++ keyboards/kyria/keymaps/mesnigan/keymap.c
@@ -11,7 +11,7 @@
       KC_CAPS, K01,     K02,     K03,     K04,     K05,                                             K06,     K07,     K08,     K09,     K0A,     KC_EQL,  \
       KC_MINS, K11,     K12,     K13,     K14,     K15,                                             K16,     K17,     K18,     K19,     K1A,     KC_QUOT, \
       KC_GRV,  K21,     K22,     K23,     K24,     K25,    _______, _______,    _______,  _______,  K26,     K27,     K28,     K29,     K2A,     KC_BSLS, \
-                                 _______, K31,     K32,    K33,     _______,    _______,  K34,      K35,     K36,     _______ \
+                                 _______, _______, K31,     K32,    K33,         K34,      K35,     K36,     _______, _______ \
   )
 /* Re-pass though to allow templates to be used */
 #define LAYOUT_kyria_base_wrapper(...)       LAYOUT_kyria_base(__VA_ARGS__)
