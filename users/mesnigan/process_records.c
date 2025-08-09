ae4fa8a7856aa1d617dc8dba430856490bd89a09
Acaki <github-20201224@wdy.one>
2025-08-10 02:56:54 +0800

    Update hold_on_other_key_press

--- users/mesnigan/process_records.c
+++ users/mesnigan/process_records.c
@@ -60,6 +60,7 @@ bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
         case THUMB_R2:
         case THUMB_R3:
         case LT(_LOWER, KC_ESC):
+        case LT(_LOWER_S, KC_ESC):
             return true;
         default:
             // Do not select the hold action when another key is pressed.
