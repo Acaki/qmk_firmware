b90c018894b161a041ebd3f76dfe54c753f88b0f
Acaki <github-20201224@wdy.one>
2026-07-26 02:45:31 +0800

    Add back charybdis/v1_elitec

---
+++ keyboards/bastardkb/charybdis/4x6/v1_elitec/config.h
@@ -0,0 +1,10 @@
+// Copyright 2020 Christopher Courtney <drashna@live.com> (@drashna)
+// Copyright 2021 Charly Delay <charly@codesink.dev> (@0xcharly)
+// SPDX-License-Identifier: GPL-2.0-or-later
+#pragma once
+
+/* Handedness. */
+#define MASTER_RIGHT
+
+/* PMW3360 settings. */
+#define POINTING_DEVICE_CS_PIN B0
