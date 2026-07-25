e623d0dc3f119f308bac7d7b9150ca1e6171489b
Mason <github-20201224@wdy.one>
2026-07-25 17:31:46 +0800

    Fix jimmy/iris

---
+++ keyboards/bastardkb/scylla/v1_elitec/config.h
@@ -0,0 +1,7 @@
+// Copyright 2021 Quentin LEBASTARD <qlebastard@gmail.com>
+// Copyright 2022 Charly Delay <charly@codesink.dev> (@0xcharly)
+// SPDX-License-Identifier: GPL-2.0-or-later
+#pragma once
+
+/* Handedness. */
+#define MASTER_RIGHT
